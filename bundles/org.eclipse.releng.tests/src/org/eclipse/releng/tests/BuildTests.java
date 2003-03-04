808f05274a6fb1b465279cb7b2bf688a0143752a
cvs2svn <platform-releng-dev@eclipse.org>
2003-03-04 03:00:55 +0000

    This commit was manufactured by cvs2svn to create tag 'v20030303'.
    Sprout from master 2003-03-04 03:00:54 UTC Sonia Dimitrov <sdimitro> 'run genHtml with 1.4 vm and xalan.jar on the cp'
    Cherrypick from master 2003-03-03 15:38:05 UTC Dean Roberts <droberts> 'Add CVS K tag tests':
        bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
    Delete:
        bundles/org.eclipse.ant.optional.junit/.project
        bundles/org.eclipse.ant.optional.junit/about.html
        bundles/org.eclipse.ant.optional.junit/build.properties
        bundles/org.eclipse.ant.optional.junit/fragment.xml
        bundles/org.eclipse.sdk.examples/.project
        bundles/org.eclipse.sdk.examples/about.html
        bundles/org.eclipse.sdk.examples/about.ini
        bundles/org.eclipse.sdk.examples/about.mappings
        bundles/org.eclipse.sdk.examples/about.properties
        bundles/org.eclipse.sdk.examples/build.properties
        bundles/org.eclipse.sdk.examples/cpl-v10.html
        bundles/org.eclipse.sdk.examples/eclipse32.gif
        bundles/org.eclipse.sdk.examples/notice.html
        bundles/org.eclipse.sdk.examples/plugin.properties
        bundles/org.eclipse.sdk.examples/plugin.xml
        bundles/org.eclipse.sdk.tests/.project
        bundles/org.eclipse.sdk.tests/about.html
        bundles/org.eclipse.sdk.tests/about.ini
        bundles/org.eclipse.sdk.tests/about.mappings
        bundles/org.eclipse.sdk.tests/about.properties
        bundles/org.eclipse.sdk.tests/build.properties
        bundles/org.eclipse.sdk.tests/cpl-v10.html
        bundles/org.eclipse.sdk.tests/eclipse32.gif
        bundles/org.eclipse.sdk.tests/notice.html
        bundles/org.eclipse.sdk.tests/plugin.properties
        bundles/org.eclipse.sdk.tests/plugin.xml
        features/org.eclipse.sdk.examples-feature/.project
        features/org.eclipse.sdk.examples-feature/build.properties
        features/org.eclipse.sdk.examples-feature/cpl-v10.html
        features/org.eclipse.sdk.examples-feature/eclipse_update_120.jpg
        features/org.eclipse.sdk.examples-feature/feature.properties
        features/org.eclipse.sdk.examples-feature/feature.xml
        features/org.eclipse.sdk.examples-feature/license.html

--- bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
+++ bundles/org.eclipse.releng.tests/src/org/eclipse/releng/tests/BuildTests.java
@@ -101,32 +101,25 @@ public class BuildTests extends TestCase {
 	}
 	
 	public void testCVSKTag() {
-		
 		boolean result = false;
 		
 		initializeCVSTypes();
 		
 		// String to use when running as an automated test.
-		String logFileName = BootLoader.getInstallURL().getPath() + ".." + File.separator + ".." + File.separator + "results" + File.separator + "chkpii";
+		String logFileName = BootLoader.getInstallURL().getPath() + ".." + File.separator + ".." + File.separator + "results" + File.separator + "cvsTags";
 
-		// String to use when running in an Eclipse runtime
-//		 String logFileName = BootLoader.getInstallURL().getPath() + "plugins" + File.separator + "org.eclipse.releng.tests_2.1.0" + File.separator + "results" + File.separator + "chkpii";
+		// String t use when running in an Eclipse runtime
+		// String logFileName = BootLoader.getInstallURL().getPath() + "plugins" + File.separator + "org.eclipse.releng.tests_2.1.0" + File.separator + "results" + File.separator + "cvsTags";
 
 		new File(logFileName).mkdirs();
 		logFileName = logFileName + File.separator + "cvsTypesLog.txt";
 
-		// Source location when running in an automated test.  This is really bad form
-		// but we really don't want to recheck out all the source.
-		String sourceDirectoryName = BootLoader.getInstallURL().getPath() +  ".." + File.separator + ".." + File.separator + ".." + File.separator + ".." + File.separator + "src";
-		
-		// Source location when running locally.  Run a source build to get it.
-//		 String sourceDirectoryName = "d:\\sourceFetch";
-		
 		try {
 
 	//		StringWriter aWriter = new StringWriter();
 			BufferedWriter aLog = new BufferedWriter(new FileWriter(logFileName));
-			File rootDirectory = new File(sourceDirectoryName);
+			String rootDirectoryName = "d:\\sourceFetch";
+			File rootDirectory = new File(rootDirectoryName);
 			result = scanCVSKTag(rootDirectory, aLog);
 	//		System.out.println(aWriter.getBuffer().toString());
 			aLog.close();
@@ -164,11 +157,6 @@ public class BuildTests extends TestCase {
 		boolean result = false;
 		
 		File[] files = aDirectory.listFiles();
-		
-		if (files == null) {
-			return result;
-		}
-		
 		for (int i = 0; i < files.length; i++) {
 			File aFile = files[i];
 			if (aFile.isDirectory()) {
@@ -246,28 +234,16 @@ public class BuildTests extends TestCase {
 			if (expectedType.equals(CVS_BINARY)) {
 				if (!entryType.equals(CVS_KB)) {
 					// Fail Binary Test
-					int start = entryName.indexOf("plugins");
-					String fileName;
-					if (start == -1) {
-						fileName = entryName;
-					} else {
-						fileName = entryName.substring(start + "plugins".length() + 1);
-					}
-					aLog.write(fileName + " should be *BINARY*");
+					aLog.write("File: " + entryName + " was expected to be be binary");
+//					System.out.println("File: " + entryName + " was expected to be be binary");
 					aLog.newLine();
 					result = true;
 				}
 			} else {
 				if (!(entryType.equals(CVS_KKV) || entryType.equals(CVS_KO))) {
 					// Fail
-					int start = entryName.indexOf("plugins");
-					String fileName;
-					if (start == -1) {
-						fileName = entryName;
-					} else {
-						fileName = entryName.substring(start + "plugins".length() + 1);
-					}
-					aLog.write(fileName + " should be *TEXT*");
+					aLog.write("File: " + entryName + " was expected to be be text");
+//					System.out.println("File: " + entryName + " was expected to be be text");
 					aLog.newLine();
 					result = true;
 				}
