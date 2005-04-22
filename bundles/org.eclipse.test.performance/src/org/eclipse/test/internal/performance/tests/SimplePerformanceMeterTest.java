cc38b637df4d933be4d92e4a095148a9658994ca
Andre Weinand <aweinand>
2005-04-22 11:07:50 +0000

    fixed #91561

--- bundles/org.eclipse.test.performance/src/org/eclipse/test/internal/performance/tests/SimplePerformanceMeterTest.java
+++ bundles/org.eclipse.test.performance/src/org/eclipse/test/internal/performance/tests/SimplePerformanceMeterTest.java
@@ -10,19 +10,25 @@
  *******************************************************************************/
 package org.eclipse.test.internal.performance.tests;
 
+import junit.framework.TestCase;
+
 import org.eclipse.test.internal.performance.OSPerformanceMeter;
 import org.eclipse.test.performance.Performance;
 import org.eclipse.test.performance.PerformanceMeter;
 
-import junit.framework.TestCase;
-
-public class PerformanceMeterFactoryTest extends TestCase {
-
+public class SimplePerformanceMeterTest extends TestCase {
+	
     public void testPerformanceMeterFactory() {
-		System.setProperty("PerformanceMeterFactory", "org.eclipse.test.performance:org.eclipse.test.internal.performance.OSPerformanceMeterFactory"); //$NON-NLS-1$ //$NON-NLS-2$
+		PerformanceMeter meter= Performance.getDefault().createPerformanceMeter("scenarioId"); //$NON-NLS-1$
 		
-		PerformanceMeter pm= Performance.getDefault().createPerformanceMeter("scenarioId"); //$NON-NLS-1$
+		assertTrue(meter instanceof OSPerformanceMeter);
+	
+		meter.start();
+		meter.stop();
 		
-		assertTrue(pm instanceof OSPerformanceMeter);
+		meter.commit();
+		
+		meter.dispose();
 	}
+
 }
